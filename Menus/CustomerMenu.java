a07e8dcd1ccc843db0188f6fe42b7582e73087bd
richardbullick <79281002+richardbullick@users.noreply.github.com>
2021-03-19 23:23:28 +0000

    Update CustomerMenu.java
    -added description
    -developed do-while loop for membership
    -changed PackageCount method name
    -various bug fixes

--- Menus/CustomerMenu.java
+++ Menus/CustomerMenu.java
@@ -8,8 +8,10 @@ import Objects.Package;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 /**
- * Created by richard on 11/03/2021
- * UPDATE PROGRAM COMMENTS ABOUT PROGRAM HERE
+ * Created by KwikNKleenCarWash on 11/03/2021
+ * This is our CustomerMenu class which is used when selecting option 1 from MainMethod class. This class takes user information
+ * when purchasing packages. It accesses Package array via BuildArrayFromFile Class, then outputs Package options and asks user to
+ * make selection. It creates a MemberCustomer or NonMemberCustomer object depending on user input. Then prints a Receipt to console
  **/
 public class CustomerMenu
 {
@@ -27,12 +29,17 @@ public class CustomerMenu
 
    protected static void CustomerMenuDisplay(){
       //Variables assigned to pass through constructor parameters to build Customer objects
+      boolean isChoiceValid = false;
       System.out.println("Please enter your Name: ");
-      customerName = kb6.nextLine();
-      //do{
-      System.out.println("Are you a member (Y/N): ");
-      member = kb6.next().toUpperCase().charAt(0);
-      //}while (!(member == 'Y') || !(member != 'N'));
+      customerName = kb6.next();
+      //do while loop to ensure a binary response for membership
+      do{
+         System.out.println("Are you a member (Y/N): ");
+         member = kb6.next().toUpperCase().charAt(0);
+         if (member == 'Y' || member == 'N') {
+            isChoiceValid = true;
+         }//if
+      }while(!isChoiceValid);
 
       System.out.println();
 
@@ -67,18 +74,19 @@ public class CustomerMenu
          }//switch
       }//do
       while ((packageOption < 1) || (packageOption > 4));
-
+      
+      System.out.println("The package you have chosen is : " + packageChoice + "\n======================\n");
       Sales.totalPurchasesPerPackage(packageOption);
 
       //If statement used to create Member or NonMember objects based on choice selection from menu above
       if (member == 'Y') {
          //create MemberCustomer and pass in variables needed to the constructor
          newMember = new MemberCustomer(customerName, packageChoice,
-                 packages[packageOption].getPrice());
+                 packages[packageOption-1].getPrice());
          //use method to print details to console
          newMember.printCustomerInfo();
          //counting the amount of times Members purchased a package
-         Sales.setMemberPackageCount(1);
+         Sales.incrementMemberPackageCount();
       }//if
       else
       {
@@ -86,14 +94,14 @@ public class CustomerMenu
          {
             //create NonMemberCustomer and pass in variables needed to the constructor
             newNonMember = new NonMemberCustomer(customerName, packageChoice,
-                    packages[packageOption].getPrice());
+                    packages[packageOption-1].getPrice());
             //use method to print details to console
             newNonMember.printCustomerInfo();
             //counting the amount of times NonMembers purchased a package
-            Sales.setNonMemberPackageCount(1);
+            Sales.incrementNonMemberPackageCount();
          }//if
       }//else
-      System.out.println("The package you have chosen is : " + packageChoice + "\n======================\n");
+      //System.out.println("The package you have chosen is : " + packageChoice + "\n======================\n");
    }//CustomerMenuDisplay()
 
 
