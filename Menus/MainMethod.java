791650f1c0c3763d902728c09d74c2a9da01cda5
richardbullick <79281002+richardbullick@users.noreply.github.com>
2021-03-25 22:53:57 +0000

    Update MainMethod.java

--- Menus/MainMethod.java
+++ Menus/MainMethod.java
@@ -3,17 +3,34 @@ package Menus;
 import java.util.Scanner;
 
 public class MainMethod {
-
+   
+   static int choice;
+   
    public static void main(String[] args) {
       Scanner keyboard = new Scanner(System.in);
-      int choice = 0;
+      
+      System.out.println("Hello, welcome to Kwik'N'Kleen");
 
       do{
-         System.out.println("Hello, welcome to KwikNKleen");
          System.out.println("Which menu would you like to view?");
          System.out.println("1. Customer menu\n2. Sales menu");
          System.out.println("3. Exit system");
-         choice = keyboard.nextInt();
+         
+         //do while loop to verify read-in from keyboard
+         boolean validInput = false;
+         do{
+            System.out.println("\nEnter 1, 2, 3:");
+            char input = keyboard.next().charAt(0);
+            if (Character.isDigit(input)) {
+               validInput = true;
+               //Assign users int options to variable read in from the keyboard
+               choice = Character.getNumericValue(input);
+            }
+            else {
+               System.out.print("Invalid entry, please enter a number.\n");
+            }
+         }while(!validInput);
+         
          switch (choice){
             case 1:
                CustomerMenu.CustomerMenuDisplay();
@@ -22,12 +39,11 @@ public class MainMethod {
                Sales.menu();
                break;
             case 3:
-               System.out.println("You have chosen to exit the system. Good bye :)");
+               System.out.println("You have chosen to exit the system.\nGoodbye");
             default:
-               System.out.println("Invalid menu choice...");
+               System.out.println(choice + " is an invalid menu choice. Please choose again.\n");
          }
       }while (choice != 3);
 
-   }
-
-}
+   }//main
+}//MainMethod
